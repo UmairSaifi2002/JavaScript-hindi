6020809f29dcf6b52516c5604e83113dcf1f0351
Umair <umairsaifi2002@gmail.com>
2024-02-20 00:53:02 +0530

    some changes

--- 01_Basics/07_nums_and_Maths.js
+++ 01_Basics/07_nums_and_Maths.js
@@ -33,9 +33,10 @@ const hundered = 1000000
 //console.log(Math.min(2,6,43,8,9))// -> 2
 //console.log(Math.max(2,6,43,8,9))// -> 43
 
-console.log(Math.random()) // -> it gives the random number between 0 and 1
+//console.log(Math.random()) // -> it gives the random number between 0 and 1
 // we want a random number between interval a and b where a,b belongs to integer value
 // so if min_val = 1 and max_val = 10
 // formula for random number between min_val and max_val
-// -> console.log(Math.ceil(min_value+(Math.random()*max_value)))
+// we can use both Math.floor() & Math.ceil() 
+//-> console.log(min_val + Math.floor(Math.rsndom()*(max_val - min_val + 1)))
 console.log(Math.ceil(1+(Math.random()*10))) // -> it return the value between 1 and 10
