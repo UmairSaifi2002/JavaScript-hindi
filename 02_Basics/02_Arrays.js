f0e87608a3e267828bd44e7e4b7516d88023ea58
UmairSaifi2002 <umairsaifi2002@gmail.com>
2024-02-22 00:07:53 +0530

    some new code

--- 02_Basics/02_Arrays.js
+++ 02_Basics/02_Arrays.js
@@ -7,4 +7,26 @@ const DC_heroes = ["Superman","Batman","flash"]
 
 // .concat() function adds two array and return the new updated array it donot change or manipulate the existing array
 const All_Heros = Marvel_heros.concat(DC_heroes) // -> here two arrays are combined and return the new updated array
-console.log(All_Heros)
+// console.log(All_Heros) // -> [ 'Ironman', 'Spiderman', 'Thor', 'Superman', 'Batman', 'flash' ]
+
+// spred operator for this just put three dots before the name of the array
+// ...ArrayName
+const All_New_Heros = [...Marvel_heros,...DC_heroes]
+// console.log(All_New_Heros) // -> [ 'Ironman', 'Spiderman', 'Thor', 'Superman', 'Batman', 'flash' ]
+
+// if we have an array within the array and this array also within the array
+const another_array = [1,2,3,[4,5,6],7,[6,7,[4,5]]] // like this worst case
+// to flatened this multidimensional array we use .flat(depth_value) function
+// .flat(depth_value) function Returns a new array with all sub-array elements concatenated into it recursively up to the specified depth.
+const another_real_array = another_array.flat(Infinity) // use infinity when depth is not known
+// console.log(another_real_array)
+
+console.log(Array.isArray("Umair"))// .isArray() checks that the given input is array or not
+console.log(Array.from("Umair")) // .from() it convert the input into the array
+console.log(Array.from({name:"Umair"})) // -> interesting
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3))
---
+++ 02_Basics/03_Objects.js
@@ -0,0 +1,82 @@
+// Object can defined by two ways
+// 1, Literal
+// 2, Constructor
+
+// Singleton property tab ati h object m jab vo constructor se bana ho
+// Singleton k mtlb k vo apne jesa ek hi object exist krta h
+
+// Let learn about Object literal
+// Here all the key's are accessible as a String to the compiler
+const JsUser = {
+    name : "Umair",
+    "FullName":"UmairSaifi",
+    age : 20,
+    location : "Jaipur",
+    email : "Umair@gmail.com",
+    isLoggedIn : false,
+    LastLoginDays : ["Monday","Saturday"]
+}
+// That's how we can access the key's & Valuesof the object.
+// There is two ways to access the content of the object.
+
+// The first method -> "Object_Name.Key"
+// console.log(JsUser.name)
+// console.log(JsUser.age)
+// console.log(JsUser.location)
+// console.log(JsUser.email)
+// console.log(JsUser.isLoggedIn)
+// console.log(JsUser.LastLoginDays)
+
+// The second one is given below
+// Object_Name["Key"] Make sure that key is within the invereted commas.
+// The Best way to access the values of the objects is this second method.
+// console.log(JsUser["name"])
+// console.log(JsUser["age"])
+// console.log(JsUser["location"])
+// console.log(JsUser["email"])
+// console.log(JsUser["isLoggedIn"])
+// console.log(JsUser["LastLoginDays"])
+
+// You cannot access the fullname value of the object with "." function
+// The Only one way to access the fullname value by the second method which is JsUser["FullName"]
+// console.log(JsUser["FullName"]) // -> UmairSaifi
+
+// That's how we declare the Symbol
+const mySymbol = Symbol("key1")
+// if we want to insert the Symbol in object then we use "[]" for declaring the key
+const Object2 = {
+    name: "Umair2",
+    age:20,
+    location:"Delhi",
+    [mySymbol]:"MyKey2" // That's how Symbol is Declared in Object
+}
+// console.log(Object2[mySymbol]) // That's how the Symbol is accessed from the Object
+// console.log(typeof mySymbol)
+
+
+// That's how we can Overwrite the values of the Key of the Object
+Object2.name = "UmairSaifi122"
+
+// if we want to freeze the object's value's
+// means if we want to make Object immutable the we use .freeze() method
+// that's how we can use .freeze() -> "Object.freeze(Object_name)"
+Object.freeze(Object2) // -> Now it is immutable
+// and if we want to make any changes to the object which is freeze it is not possible to change
+// and we donot get any also
+
+// console.log(JsUser)
+// console.log(Object2)
+
+// we can add the function to the object
+JsUser.greeting = function(){
+    console.log("Hello ",JsUser["name"])
+}
+console.log(JsUser.greeting) // -> [Function (anonymous)]
+console.log(JsUser.greeting()) 
+JsUser.greeting()
+
+JsUser.greetingtwo = function(){
+    console.log(`Hello Js User , ${this.name}`) // Here this() is used because we want the value of the same
+    // Object in which the function is defined
+}
+JsUser.greetingtwo()
