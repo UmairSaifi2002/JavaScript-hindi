f778cdaec76bee9bb77f3fc06f72131a6c0a8f7c
UmairSaifi2002 <umairsaifi2002@gmail.com>
2024-02-25 22:14:50 +0530

    Create 04_Objects.js

---
+++ 02_Basics/04_Objects.js
@@ -0,0 +1,3 @@
+// In this We will learn Object by contructor method
+// here Singleton property is imposed on the object because we use the consstructor method to define the object
+const newUser = new Object()
