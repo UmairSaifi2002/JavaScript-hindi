855d74c70a431ad67208ef4d1fbd0b993ea7a106
UmairSaifi2002 <umairsaifi2002@gmail.com>
2024-02-26 23:01:52 +0530

    Update 04_Objects.js

--- 02_Basics/04_Objects.js
+++ 02_Basics/04_Objects.js
@@ -1,3 +1,9 @@
 // In this We will learn Object by contructor method
 // here Singleton property is imposed on the object because we use the consstructor method to define the object
-const newUser = new Object()
+const tinderUser = new Object()
+console.log(tinderUser) // -> {}
+// Now we will give some values to the object
+tinderUser.id = "123abc"
+tinderUser.name = "sam"
+tinderUser.IsLoggedin = false
+console.log(tinderUser) // -> { id: '123abc', name: 'sam', IsLoggedin: false }
