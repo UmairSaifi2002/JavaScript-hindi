37bef31d3bb13836c0440cf5f75fbf4db931b9fb
UmairSaifi2002 <umairsaifi2002@gmail.com>
2024-02-27 07:01:41 +0530

    Update 04_Objects.js

--- 02_Basics/04_Objects.js
+++ 02_Basics/04_Objects.js
@@ -29,10 +29,44 @@ const obj1 = {1:"a",2:"b"}
 const obj2 = {3:"c",4:"d"}
 const obj3 = {5:"e",6:"f"}
 const obj4 = {7:"g",8:"h"}
+
+
 // now if we want to combine both the objects
 // we can use the -> 'Object.assign()' function
 // Syntax -> Object.assign(target,source) here source can either one or more than one object
 // if we have more than one object then -> Object.assign(target,source1,source2,source3,...)
 
-const finalObject = Object.assign({},obj1,obj2,obj3,obj4)
-console.log(finalObject) // -> {1:'a',2:'b',3:'c',4:'d',5:'e',6:'f',7:'g',8:'h'}
+// const finalObject = Object.assign({},obj1,obj2,obj3,obj4)
+// console.log(finalObject) // -> {1:'a',2:'b',3:'c',4:'d',5:'e',6:'f',7:'g',8:'h'}
+
+// The another usefull solution to combine tw or more than objects
+// to use the spread operator in object 
+// {...Objectname,...Objectname, and so on}
+const finalObject = {...obj1,...obj2,...obj3,...obj4}
+// console.log(finalObject) // -> {1:'a',2:'b',3:'c',4:'d',5:'e',6:'f',7:'g',8:'h'}
+
+
+// if we have a array of object
+const users = [
+    {1:"a",email:"a@example.com",packageName:"firstUser"},
+    {1:"b",email:"b@example.com",packageName:"SecondUser"},
+    {1:"c",email:"c@example.com",packageName:"thirdUser"},
+    {1:"d",email:"d@example.com",packageName:"fourthUser"}
+]
+// now we can acees the first object of the array
+console.log(users[1]) // -> { '1': 'b', email: 'b@example.com', packageName: 'SecondUser' }
+// and if we want to access the particular value of the first object or any other object then
+console.log(users[1].email) // -> b@example.com
+
+// if we want to access all the keys or the value of the object then
+// for keys -> Object.keys(object_name)
+// for values -> Object.values(object_name)
+// the most important thing is it return array as an output
+console.log(Object.keys(tinderUser)) // -> [ 'id', 'name', 'IsLoggedin' ]
+console.log(Object.values(tinderUser)) // -> [ '123abc', 'sam', false ]
+
+// now if we want keys along with its value then we use '.entries()' function
+// Let's see how can we use it
+// Syntax -> Object.entries(object_name)
+// it will return an nested array
+console.log(Object.entries(tinderUser)) // -> [ [ 'id', '123abc' ], [ 'name', 'sam' ], [ 'IsLoggedin', false ] ]
