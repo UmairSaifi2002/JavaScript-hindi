439e3849d714a37adfc89d3e62c69e0264372c72
UmairSaifi2002 <umairsaifi2002@gmail.com>
2024-03-16 00:29:48 +0530

    BMI complete

--- 07_Some_small_Project/02_BMI_Calculator/BMI_calculator.js
+++ 07_Some_small_Project/02_BMI_Calculator/BMI_calculator.js
@@ -3,7 +3,7 @@ const form = document.querySelector('form');
 // if we select the input field before the eventlister then whenever the eventlistner is called or button is clicked
 // then page will be reload and with this the valus withih the input field is null
 
-form.addEventListener('submit',(e)=>{
+form.addEventListener('submit',function (e){
     e.preventDefault();
 
     // the only way to get values from input fields
@@ -11,4 +11,16 @@ form.addEventListener('submit',(e)=>{
     // we have to select the input field and hold the values in a variable for furthur use
     const height = parseInt(document.querySelector('#height').value);
     const weight = parseInt(document.querySelector('#weight').value);
+    const results = document.querySelector('#result');
+
+    if(height === '' || height < 0 || isNaN(height)){
+        results.innerHTML = 'Please give a valid Input!';
+        results.style.padding = '8px';
+    }
+    else{
+        // here we use '.toFixed(position)' method to display the floating number upto needed position.
+        const bmi = (weight / ((height/100)*(height/100))).toFixed(2);
+        results.innerHTML = `Your BMI Score is : ${bmi}`;
+        results.style.padding = '8px';
+    }
 })
